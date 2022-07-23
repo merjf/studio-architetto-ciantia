3f93fa65af1c14899e2c74881911aaf784a4723c
Francesco Mergiotti <francescomergiotti.25@gmail.com>
2022-07-23 21:06:34 +0200

    First Project model adjustment

--- src/assets/data/project.ts
+++ src/assets/data/project.ts
@@ -1,33 +1,55 @@
 import ProjectClass from '@/models/models'
 
 const projects = [{
-    id: 1001,
-    title: "Frette - Agira",
-    description: "Collaborazione con Filippo",
-    place: "Sicilia Outlet Village - Agira EN",
-    mainFolder: "frette-agira",
-    imageNumber: 13
+    name: "Restauro",
+    type: "group",
+    values: [{
+            id: 1001,
+            title: "Frette - Agira",
+            description: "Collaborazione con Filippo",
+            place: "Sicilia Outlet Village - Agira EN",
+            mainFolder: "frette-agira",
+            imageNumber: 13
+        },{
+            id: 1002,
+            title: "Noventa",
+            description: "Descrizione Progetto Noventa",
+            place: "Via xxx 1234, Noventa di Piave VE",
+            mainFolder: "noventa",
+            imageNumber: 10,
+        },{
+            id: 1003,
+            title: "Palco Teatro Antico di Catania",
+            description: "Descrizione Progetto Teatro Antico di Catania",
+            place: "Via Vittorio Emanuele II 266, Catania CT",
+            mainFolder: "palco-teatro-antico-catania",
+            imageNumber: 15
+        }],
+    subgroup: undefined
 },{
-    id: 1002,
-    title: "Noventa",
-    description: "Descrizione Progetto Noventa",
-    place: "Via xxx 1234, Noventa di Piave VE",
-    mainFolder: "noventa",
-    imageNumber: 10
-// },{
-//     id: 1003,
-//     title: "Negozio Abbigliamento Luxury",
-//     description: "Descrizione Progetto Negozio Abbigliamento Luxury",
-//     place: "Via Vittorio Emanuele II 266, Catania CT",
-//     mainFolder: "palco-teatro-antico-catania",
-//     imageNumber: 15
-},{
-    id: 1004,
-    title: "Appartamento privato",
-    description: "Descrizione Progetto Appartamento privato",
-    place: "Via xxx 1234, Piazza Armerina EN",
-    mainFolder: "tony",
-    imageNumber: 25
-}];
+    name: "Interiors",
+    type: "group",
+    values: [{
+        id: 1004,
+        title: "Appartamento privato",
+        description: "Descrizione Progetto Appartamento privato",
+        place: "Via xxx 1234, Piazza Armerina EN",
+        mainFolder: "tony",
+        imageNumber: 25
+    }],
+    subgroup: {
+        name: "Product Design",
+        type: "grid",
+        values: [{
+            id: 1004,
+            title: "Appartamento privato",
+            description: "Descrizione Progetto Appartamento privato",
+            place: "Via xxx 1234, Piazza Armerina EN",
+            mainFolder: "tony",
+            imageNumber: 25
+        }], 
+    }
+}
+];
 
 export default projects as unknown as ProjectClass[];
--- src/models/models.ts
+++ src/models/models.ts
@@ -1,8 +1,14 @@
-export default class ProjectModel {
+class Project {
     id: number | undefined;
     title: string | undefined
     description: string | undefined;
     place: string | undefined;
     mainFolder: string | undefined;
     imageNumber: number | undefined;
+}
+export default class ProjectModel {
+    name: string | undefined;
+    type: string | undefined;
+    values: Project[] | undefined;
+    subgroup: ProjectModel | undefined;
 }
